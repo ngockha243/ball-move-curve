47c68a8d4fcccfac26b4fe5c334c7d80bae8681b
Ngoc Kha <khanguyen.jobs@gmail.com>
2022-10-20 15:30:52 +0700

    change shoot Ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,10 +10,22 @@ public class Ball : MonoBehaviour
 
 	public float releaseTime = .15f;
 	public float maxDragDistance = 1.5f;
+	public float Force = 15f;
 
 	public GameObject nextBall;
 
 	private bool isPressed = false;
+	public GameObject PointPrefab;
+	public GameObject[] Points;
+	public int numberOfPoints;
+	Vector2 Direction;
+	
+	void Start(){
+		Points = new GameObject[numberOfPoints];
+		for(int i = 0; i<numberOfPoints; i++){
+			Points[i] = Instantiate(PointPrefab, transform.position, Quaternion.identity);
+		}
+	}
 
 	void Update ()
 	{
@@ -25,33 +37,53 @@ public class Ball : MonoBehaviour
 				rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
 			else
 				rb.position = mousePos;
+
+			for (int i = 0; i < Points.Length; i++){
+				Points[i].transform.position = PointPosition(i * 0.1f);
+			}
 		}
+
+		Vector2 heading = hook.position - (Vector2)transform.position;
+		float distance = heading.magnitude;
+		Direction = heading/distance;
 	}
 
 	void OnMouseDown ()
 	{
 		isPressed = true;
-		rb.isKinematic = true;
+		GetComponent<SpringJoint2D>().enabled = false;
+		// rb.isKinematic = true;
+		
 	}
 
 	void OnMouseUp ()
 	{
 		isPressed = false;
-		rb.isKinematic = false;
-
+		GetComponent<SpringJoint2D>().enabled = false;
+		gameObject.GetComponent<Rigidbody2D>().velocity = (Direction) * (hook.position - (Vector2)transform.position) * Force;
+		// rb.isKinematic = false;
 		StartCoroutine(Release());
 	}
 
 	IEnumerator Release ()
 	{
+
+		for (int i = 0; i < Points.Length; i++){
+			Destroy(Points[i]);
+		}
 		yield return new WaitForSeconds(releaseTime);
 
-		GetComponent<SpringJoint2D>().enabled = false;
+		// GetComponent<SpringJoint2D>().enabled = false;
+		
 		this.enabled = false;
 
 		yield return new WaitForSeconds(.25f);
         nextBall.SetActive(true);
 		
-	
+	}
+
+	Vector2 PointPosition(float t){
+		Vector2 currentPointPos = (Vector2)transform.position + ((Direction).normalized * Force * t	)* (hook.position - (Vector2)transform.position) + 0.5f*Physics2D.gravity *(t*t);
+		return currentPointPos;
 	}
 }
