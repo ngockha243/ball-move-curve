60590335f8a1ceccbfd7c0c35419adbfc2d2cd13
Ngoc Kha <khanguyen.jobs@gmail.com>
2022-10-21 09:24:17 +0700

    update movement of ball by formula, not by physics

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
+	
+	// Rigidbody of Ball
     public Rigidbody2D rb;
-	public Rigidbody2D hook;
 
-	public float releaseTime = .15f;
+	// Get position of Hook
+	private Vector2 hookPos;
+
+	public float releaseTime = .25f;
 	public float maxDragDistance = 1.5f;
 	public float Force = 15f;
 
@@ -16,56 +20,76 @@ public class Ball : MonoBehaviour
 
 	private bool isPressed = false;
 	public GameObject PointPrefab;
-	public GameObject[] Points;
+	private GameObject[] Points;
 	public int numberOfPoints;
-	Vector2 Direction;
+	private Vector2 Direction;
+	public bool shoot = false;
+	public float timeUpdate;
+	public Vector2 rootPos;
 	
 	void Start(){
+		// Create list of prediction location of BALL
 		Points = new GameObject[numberOfPoints];
 		for(int i = 0; i<numberOfPoints; i++){
 			Points[i] = Instantiate(PointPrefab, transform.position, Quaternion.identity);
 		}
+		GameObject hook = GameObject.FindGameObjectWithTag("hook");
+		hookPos = (Vector2)hook.transform.position;
 	}
 
 	void Update ()
 	{
+		// Drag Ball -> Update Ball position when Drag, but limit distance drag 
 		if (isPressed)
 		{
 			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-			if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
-				rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+			// Limit distance drag
+			if (Vector3.Distance(mousePos, hookPos) > maxDragDistance)
+				rb.position = hookPos + (mousePos - hookPos).normalized * maxDragDistance;
 			else
 				rb.position = mousePos;
 
+			// Predict shoot movement
 			for (int i = 0; i < Points.Length; i++){
 				Points[i].transform.position = PointPosition(i * 0.1f);
 			}
 		}
+		// When shoot, Update shoot movement of Ball by formula
+		if(!shoot){
+			// Get direction when shoot
+			Vector2 heading = hookPos - (Vector2)transform.position;
+			float distance = heading.magnitude;
+			Direction = heading/distance;
+			rootPos = transform.position;
+		}
+		else{
+			timeUpdate += Time.deltaTime;
+			gameObject.transform.position = (Vector2)rootPos + ((Direction).normalized * Force * timeUpdate * (hookPos - (Vector2)rootPos).magnitude + 0.5f*Physics2D.gravity *(timeUpdate * timeUpdate));
+		}
 
-		Vector2 heading = hook.position - (Vector2)transform.position;
-		float distance = heading.magnitude;
-		Direction = heading/distance;
 	}
 
 	void OnMouseDown ()
 	{
 		isPressed = true;
-		GetComponent<SpringJoint2D>().enabled = false;
-		// rb.isKinematic = true;
-		
+		// GetComponent<SpringJoint2D>().enabled = false;
+
+		// Remove Drag force when mouse down
+		rb.isKinematic = true;
 	}
 
 	void OnMouseUp ()
 	{
+		shoot = true;
 		isPressed = false;
-		GetComponent<SpringJoint2D>().enabled = false;
-		gameObject.GetComponent<Rigidbody2D>().velocity = (Direction) * (hook.position - (Vector2)transform.position).magnitude * Force;
-		// rb.isKinematic = false;
+		// GetComponent<SpringJoint2D>().enabled = false;
+		// Option 1: Update shoot movement by Velocity
+		// gameObject.GetComponent<Rigidbody2D>().velocity = (Direction) * (hook.position - (Vector2)transform.position).magnitude * Force;
+		rb.isKinematic = false;
 		StartCoroutine(Release());
 	}
 
-	IEnumerator Release ()
+	IEnumerator Release()
 	{
 
 		for (int i = 0; i < Points.Length; i++){
@@ -74,16 +98,18 @@ public class Ball : MonoBehaviour
 		yield return new WaitForSeconds(releaseTime);
 
 		// GetComponent<SpringJoint2D>().enabled = false;
-		
-		this.enabled = false;
+		// this.enabled = false;
 
-		yield return new WaitForSeconds(.25f);
+		yield return new WaitForSeconds(releaseTime);
         nextBall.SetActive(true);
 		
 	}
 
+	void OnCollisionEnter2D(Collision2D other){
+		shoot = false;
+	}
 	Vector2 PointPosition(float t){
-		Vector2 currentPointPos = (Vector2)transform.position + ((Direction).normalized * Force * t	)* (hook.position - (Vector2)transform.position).magnitude + 0.5f*Physics2D.gravity *(t*t);
+		Vector2 currentPointPos = (Vector2)transform.position + ((Direction).normalized * Force * t	)* (hookPos - (Vector2)transform.position).magnitude + 0.5f*Physics2D.gravity *(t*t);
 		return currentPointPos;
 	}
 }
